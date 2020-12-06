4a0393213668769962d5caa495605ebfb216292b
eduuh <edwinkamaumuraya0@outlook.com>
2020-12-06 09:45:42 +0300

    starting on shell to driver

--- shelltodriver/backend/routes/products.js
+++ shelltodriver/backend/routes/products.js
@@ -1,4 +1,6 @@
 const Router = require('express').Router;
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
 const router = Router();
 
@@ -9,7 +11,7 @@ const products = [
     description:
       'A stylish backpack for the modern women or men. It easily fits all your stuff.',
     price: 79.99,
-    image: 'http://localhost:3100/images/product-backpack.jpg'
+    image: 'http://localhost:3100/images/product-backpack.jpg',
   },
   {
     _id: 'asdgfs1',
@@ -17,7 +19,7 @@ const products = [
     description:
       "How could a man resist these lovely earrings? Right - he couldn't.",
     price: 129.59,
-    image: 'http://localhost:3100/images/product-earrings.jpg'
+    image: 'http://localhost:3100/images/product-earrings.jpg',
   },
   {
     _id: 'askjll13',
@@ -25,14 +27,14 @@ const products = [
     description:
       'Yes, you got that right - this MacBook has the old, working keyboard. Time to get it!',
     price: 1799,
-    image: 'http://localhost:3100/images/product-macbook.jpg'
+    image: 'http://localhost:3100/images/product-macbook.jpg',
   },
   {
     _id: 'sfhjk1lj21',
     name: 'Red Purse',
     description: 'A red purse. What is special about? It is red!',
     price: 159.89,
-    image: 'http://localhost:3100/images/product-purse.jpg'
+    image: 'http://localhost:3100/images/product-purse.jpg',
   },
   {
     _id: 'lkljlkk11',
@@ -40,15 +42,15 @@ const products = [
     description:
       'Never be naked again! This T-Shirt can soon be yours. If you find that buy button.',
     price: 39.99,
-    image: 'http://localhost:3100/images/product-shirt.jpg'
+    image: 'http://localhost:3100/images/product-shirt.jpg',
   },
   {
     _id: 'sajlfjal11',
     name: 'Cheap Watch',
     description: 'It actually is not cheap. But a watch!',
     price: 299.99,
-    image: 'http://localhost:3100/images/product-watch.jpg'
-  }
+    image: 'http://localhost:3100/images/product-watch.jpg',
+  },
 ];
 
 // Get list of products products
@@ -69,7 +71,7 @@ router.get('/', (req, res, next) => {
 
 // Get single product
 router.get('/:id', (req, res, next) => {
-  const product = products.find(p => p._id === req.params.id);
+  const product = products.find((p) => p._id === req.params.id);
   res.json(product);
 });
 
@@ -80,8 +82,18 @@ router.post('', (req, res, next) => {
     name: req.body.name,
     description: req.body.description,
     price: parseFloat(req.body.price), // store this as 128bit decimal in MongoDB
-    image: req.body.image
+    image: req.body.image,
   };
+
+  mongodb
+    .connect(
+      'mongodb+srv://eduuh:4bGisSJaq2rKSb5q@cluster0.aslmt.azure.mongodb.net/shop?retryWrites=true&w=majority'
+    )
+    .then((client) => {
+      console.log('Connected');
+      client.db().collection('products').insertOne(newProduct);
+    })
+    .catch((err) => console.log(err));
   console.log(newProduct);
   res.status(201).json({ message: 'Product added', productId: 'DUMMY' });
 });
@@ -93,7 +105,7 @@ router.patch('/:id', (req, res, next) => {
     name: req.body.name,
     description: req.body.description,
     price: parseFloat(req.body.price), // store this as 128bit decimal in MongoDB
-    image: req.body.image
+    image: req.body.image,
   };
   console.log(updatedProduct);
   res.status(200).json({ message: 'Product updated', productId: 'DUMMY' });
